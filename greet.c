74b8d230c5718e4a0f870f27a5fd53a1f116ab91
STADD <stadd@windowslive.com>
2018-10-08 15:52:07 +0200

    fixed greet.c

--- greet.c
+++ greet.c
@@ -3,19 +3,16 @@
 
 int main()
 {
-<<<<<<< HEAD:greet.c
     char name[20] = "Balazs";
 	int age = 19;
     return 0;
 }
 
-void greet(char name){
+void greet(char name, int age){
     printf("Name: %s\t Age: %d", name, age);
-=======
     greet();
 }
 
 void greet(char name){
     printf("Greetings");
->>>>>>> 2b7578df39a8bd0f48a8fbfdd93ca97057866ec9:greet/main.c
 }
